47fae7b619f5a809e5448b32606a96e11678da7b
Andres-Dead <2118100663@soy.utj.edu.mx>
2021-10-24 01:46:48 -0500

    login semicomplete on front end

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './styles/main.scss';
+import Axios from 'axios';
+import React, {useState} from 'react';
+
 
 const switchers = [...document.querySelectorAll('.switcher')]
 
@@ -9,12 +12,65 @@ switchers.forEach(item => {
 	})
 })
 
+
+
+
 function App() {
+
+  //Login
+  const [EmailLog, setEmailLog] = useState("")
+  const [passwordLog, setPasswordLog] = useState("")
+
+  const login = () =>{
+    Axios.post('http://localhost:3000/login', 
+    {
+      mail: EmailLog,
+      password: passwordLog
+    }).then((response)=>{
+      if(response.data.message){
+        setLoginStatus(response.data.message)  
+      }else{
+        setLoginStatus(response.data[0].firstName) 
+      }
+    })
+  }
+
+  //error msg
+  const [loginStatus, setLoginStatus] = useState("");
+
+  //register
+  const [usernameReg, setUserNameReg] = useState("") 
+  const [userLastNameReg, setUserLastNameReg] = useState("")
+  const [userPhoneReg, setUserPhoneReg] = useState("")
+  const [userRoleReg, setUserRoleReg] = useState("")
+  const [EmailReg, setEmailReg] = useState("")
+  const [passwordReg, setPasswordReg] = useState("")
+  
+  const register = () =>{
+    Axios.post('http://localhost:3000/register', 
+    {
+      firstName: usernameReg,
+      lastName: userLastNameReg,
+      phone: userPhoneReg,
+      role: userRoleReg,
+      mail: EmailReg,
+      password: passwordReg
+    }).then((response)=>{
+      console.log(response)
+    })
+  }
+  
+
+  
+
+
   return (
     <div className="App">
       <section className="forms-section">
         <h1 class="section-title">Vencejos</h1>
         <div className="forms">
+
+          {/*Login*/}
           <div className="form-wrapper is-active">
             <button type="button" className="switcher switcher-login">
               Ingresar
@@ -25,38 +81,89 @@ function App() {
                 <legend>Ingresa tu correo y contraseña.</legend>
                 <div className="input-block">
                   <label for="login-email">Correo</label>
-                  <input id="login-email" type="email" required/>
+                  <input id="login-email" type="email" required 
+                  onChange={(e)=> {
+                    setEmailLog(e.target.value)
+                    }}/>
                 </div>
                 <div className="input-block">
                   <label for="login-password">Contraseña</label>
-                  <input id="login-password" type="password" required/>
+                  <input id="login-password" type="password" required
+                  onChange={(e)=> {
+                    setPasswordLog(e.target.value)
+                    }}/>
                 </div>
               </fieldset>
-              <button type="submit" className="btn-login">Ingresar</button>
+              <button type="submit" className="btn-login" 
+              onClick={login}>Ingresar</button>
             </form>
+
+            <h1>{loginStatus}</h1>
           </div>
+
+
+
+
+          {/*Register*/}
           <div className="form-wrapper">
             <button type="button" className="switcher switcher-signup">
-               Registrarse
+               Solicitar Acceso
                 <span className="underline"></span>
               </button>
               <form className="form form-signup">
                 <fieldset>
-                  <legend>Ingresa tu corre y contraseña.</legend>
-                  <div class="input-block">
+                  <legend>Ingresa los datos solicitados.</legend>
+                  <div className="input-block">
+                    <label for="signup-name">Nombre</label>
+                    <input id="signup-name" type="text" required 
+                    onChange={(e)=> {
+                      setUserNameReg(e.target.value)
+                      }}/>
+                  </div>
+                  <div className="input-block">
+                    <label for="signup-name">Apellido</label>
+                    <input id="signup-name" type="text" required 
+                    onChange={(e)=> {
+                      setUserLastNameReg(e.target.value)
+                      }}/>
+                  </div>
+                  <div className="input-block">
+                    <label for="signup-phone">Telefono</label>
+                    <input id="signup-phone" type="phone" required 
+                    onChange={(e)=> {
+                      setUserPhoneReg(e.target.value)
+                      }}/>
+                  </div>
+                  <div className="input-block">
                     <label for="signup-email">Correo</label>
-                    <input id="signup-email" type="email" required/>
+                    <input id="signup-email" type="email" required 
+                    onChange={(e)=> {
+                      setEmailReg(e.target.value)
+                      }}/>
                   </div>
-                  <div class="input-block">
+                  <div className="input-block">
                     <label for="signup-password">Contraseña</label>
-                    <input id="signup-password" type="password" required/>
+                    <input id="signup-password" type="password" required
+                    onChange={(e)=> {
+                      setPasswordReg(e.target.value)
+                      }}/>
                   </div>
-                  <div class="input-block">
+                  <div className="input-block">
                     <label for="signup-password-confirm">Confirmar contraseña</label>
                     <input id="signup-password-confirm" type="password" required/>
                   </div>
+                  <div className="input-block">
+                    <label for="signup-role">Rol</label>
+                    <select name="role"  
+                    onChange={(e)=> {
+                      setUserRoleReg(e.target.value)
+                      }}>
+                      <option value="employee">Empleado</option>
+                      <option value="client">Cliente</option>
+                    </select>
+                  </div>
                 </fieldset>
-                <button type="submit" class="btn-signup">Continuar</button>
+                <button type="submit" className="btn-signup" onClick={register}>Solicitar</button>
               </form>
           </div>
         </div>
