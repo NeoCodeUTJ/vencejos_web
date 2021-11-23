46f8a500c4c682b6cfb0aeaab8c80ac5c1d6e3da
Carlos Cendejas <carloscb02@outlook.com>
2021-11-23 00:47:57 -0600

    finished: list users

--- src/components/users/ListUser.js
+++ src/components/users/ListUser.js
@@ -5,6 +5,7 @@ import CardBody from '@material-tailwind/react/CardBody';
 import Button from '@material-tailwind/react/Button';
 import InputIcon from "@material-tailwind/react/InputIcon";
 import { getUsers } from 'services/users.service';
+import { data } from 'autoprefixer';
 
 const ListUsers = () => {
 
@@ -12,9 +13,8 @@ const ListUsers = () => {
 
     useEffect(() => {
         getUsers()
-            .then(({data}) => {
-                setResponse(data);
-                console.log("Respuesta: ", response);
+            .then(({ data }) => {
+                setResponse(data.data);
             })
             .catch(error => {
                 console.log(error);
@@ -64,6 +64,9 @@ const ListUsers = () => {
                                     <thead>
                                         <tr>
                                             <th className="px-2 text-pink-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-medium text-center">
+                                                ID
+                                            </th>
+                                            <th className="px-2 text-pink-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-medium text-center">
                                                 Nombre
                                             </th>
                                             <th className="px-2 text-pink-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-medium text-center ">
@@ -73,32 +76,40 @@ const ListUsers = () => {
                                                 Rol
                                             </th>
                                             <th className="px-2 text-pink-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-medium text-center">
-                                                Municipio
-                                            </th>
-                                            <th className="px-2 text-pink-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-medium text-center">
                                                 Correo electrónico
                                             </th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            {/*Nombre*/}
-                                            <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
 
-                                            </th>
-                                            {/*Celular*/}
-                                            <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                        {response?.map((element, index) => {
+                                            console.log(element.email)
+                                            return (
+                                                <tr key={index}>
+                                                     {/*ID*/}
+                                                     <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                                        #{element.id}
+                                                    </th>
+                                                    {/*Nombre*/}
+                                                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                                        {element.name} {element.first_surname} {element.second_surname}
+                                                    </th>
+                                                    {/*Celular*/}
+                                                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                                        {element.phone}
+                                                    </th>
+                                                    {/*Rol*/}
+                                                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                                        {element.role}
+                                                    </th>
+                                                    {/*Correo*/}
+                                                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                                        {element.email}
+                                                    </th>
+                                                </tr>
+                                            )
+                                        })}
 
-                                            </th>
-                                            {/*Rol*/}
-                                            <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
-
-                                            </th>
-                                            {/*Correo*/}
-                                            <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
-
-                                            </th>
-                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
